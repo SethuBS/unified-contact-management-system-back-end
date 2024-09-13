1b7273b3dc25e529a5b25e5eb2c540b22bf6654f
Sethu <sethuserge@gmail.com>
2024-09-13 10:42:23 +0200

    missing line code

--- src/main/java/com/ucms/backend/exception/ContactIllegalArgumentException.java
+++ src/main/java/com/ucms/backend/exception/ContactIllegalArgumentException.java
@@ -2,6 +2,6 @@ package com.ucms.backend.exception;
 
 public class ContactIllegalArgumentException extends RuntimeException {
     public ContactIllegalArgumentException(String message) {
-
+        super(message);
     }
 }
