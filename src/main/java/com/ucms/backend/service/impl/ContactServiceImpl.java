a77609cb1b1aa95f158c90f617dfdc28ee9fad77
Sethu <sethuserge@gmail.com>
2024-09-25 16:54:52 +0200

    Features: searchByMultipleFields Pagination Testing

--- src/main/java/com/ucms/backend/service/impl/ContactServiceImpl.java
+++ src/main/java/com/ucms/backend/service/impl/ContactServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
